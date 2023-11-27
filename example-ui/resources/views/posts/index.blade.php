a9a5433917e2789484cab17bddb678bb1cfb3a7f
Mert Kaynak <kaynak.mert35@gmail.com>
2023-11-28 00:13:13 +0300

    Posts Page

---
+++ example-ui/resources/views/posts/index.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Yazılar</title>
+</head>
+
+<body>
+    <h1>Yazılar</h1>
+    <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Porro doloremque officiis laboriosam officia ducimus.
+        Eius, nemo. Hic consequuntur pariatur molestiae.
+    </p>
+</body>
+
+</html>
